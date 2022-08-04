fccb36d7173fad5295ca82ccf98e8297e09153d7
Mobin Ghaemi <105428684+mobin-ghaemi@users.noreply.github.com>
2022-08-04 14:27:12 +0430

    Add Description

--- isOnDesktop.sh
+++ isOnDesktop.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-
-# this script return true if the pointer is on the root window.
-# requires xorg-xwininfo and xdotool.
-
+#This script is to find out whether you are on the desktop or not
 rootwin=$(xwininfo -root | grep -Po 'Window id: \K\S+')
 currentwin=$(printf '0x%x\n' "$(xdotool getmouselocation | cut -d: -f5)")
 [ -n "$currentwin" ] && [ "$currentwin" = "$rootwin" ]
